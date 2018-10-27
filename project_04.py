c5c7a60722ee1ef2df64709be95b0b9b32e1a21b
mashabat2 <mashabat2@mail.ru>
2018-10-27 16:21:16 +0700

    2 part of my job

--- project_04.py
+++ project_04.py
@@ -1,3 +1,21 @@
-# Introduction
-
-# Body
+while True:
+    x = int(input('Для начала игры введите 0: '))
+    if x == 0:
+        text = str(input('Введите текст на русском: '))
+        symbol = str(input('Введите символ: '))
+        punctuation = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+        consonant = 'бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ' # согласные
+        vowel = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ' # гласные
+        s = None
+        a = []
+        for w in text:
+            if w in punctuation:
+                s = w
+            elif w in vowel:
+                s = w + symbol + w.lower()
+            if w in consonant:
+                s = w
+            a += s
+        print("".join(a))
+    if x != 0:
+        break
