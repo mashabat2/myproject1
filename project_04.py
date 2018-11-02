addd3879ddaf42070bff28a3fc95e876f031a5e9
mashabat2 <mashabat2@mail.ru>
2018-11-02 21:53:54 +0700

    2 change of my project

--- project_04.py
+++ project_04.py
@@ -1,11 +1,11 @@
 while True:
-    x = int(input('Для начала игры введите 0: '))
+    x = int(input('To start the game print 0: '))
     if x == 0:
-        text = str(input('Введите текст на русском: '))
-        symbol = str(input('Введите символ: '))
+        text = str(input('Please enter the text in Russian language: '))
+        symbol = str(input('Please enter the symbol: '))
         punctuation = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
-        consonant = 'бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ' # согласные
-        vowel = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ' # гласные
+        consonant = 'бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ'  # согласные
+        vowel = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ'  # гласные
         s = None
         a = []
         for w in text:
